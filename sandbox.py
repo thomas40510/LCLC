c51e0898b3580143c9d38fca2e8bb9f0eeff532d
Thomas Prévost <thomasprevost85@gmail.com>
2022-04-28 10:56:06 +0200

    update doc s2

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,15 @@
 print("hello world!")
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+    return True
+
+
+# 1 to 100
+L = [x for x in range(1, 100) if is_prime(x)]
+print(len(L))
