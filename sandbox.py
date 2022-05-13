0aefe5c8f254ce9dffc6ae59f85164b5da8313bc
Thomas Prévost <thomasprevost85@gmail.com>
2022-05-13 11:23:31 +0200

    séance 3

--- sandbox.py
+++ sandbox.py
@@ -11,5 +11,12 @@ def is_prime(n):
 
 
 # 1 to 100
-L = [x for x in range(1, 100) if is_prime(x)]
-print(len(L))
+# L = [x for x in range(1, 100) if is_prime(x)]
+# print(len(L))
+
+v = 180
+t = 0
+while v < 340:
+    t += 1
+    v += 10
+print(v, t)
